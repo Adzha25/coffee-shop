685f2cffae2ce609bdc24078faf8d414cef32859
Adzha25 <adzhapratama23@gmail.com>
2025-06-28 22:43:45 +0700

    Perbaikan error

--- coffee-backend/server.js
+++ coffee-backend/server.js
@@ -10,6 +10,8 @@ const app = express();
 // Middleware
 app.use(cors({
   origin: ['http://localhost:5173', 'https://coffee-shop-production-e6b2.up.railway.app'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 }));
 app.use(express.json());
