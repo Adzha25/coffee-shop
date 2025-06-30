f141fd67c21e856691a0eea4261f19503f47d419
Adzha25 <adzhapratama23@gmail.com>
2025-06-30 18:57:31 +0700

    Perbaikan bug login

--- coffee-backend/server.js
+++ coffee-backend/server.js
@@ -7,6 +7,11 @@ const orderRoutes = require('./routes/orderRoutes');
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log('[REQUEST]', req.method, req.path, req.headers.origin);
+  next();
+});
+
 // Middleware
 app.use(cors({
   origin: ['http://localhost:5173', 'https://coffee-shop-production-e6b2.up.railway.app'],
