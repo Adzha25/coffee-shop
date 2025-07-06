d808a24ac0fcfa981ed45a2e02da5c69cc165743
Adzha25 <adzhapratama23@gmail.com>
2025-07-06 21:36:28 +0700

    commit

--- coffee-backend/server.js
+++ coffee-backend/server.js
@@ -30,11 +30,19 @@ mongoose.connect(process.env.MONGO_URI)
   .catch(err => console.error('❌ MongoDB Connection Error:', err));
 
 // Routing
+console.log('[DEBUG] Mounting routes...');
+
 app.use('/api/orders', orderRoutes);
 app.use('/api/admin', require('./routes/adminRoutes'));
+app.use((req, res, next) => {
+  console.log(`[DEBUG ROUTE] ${req.method} ${req.path}`);
+  next();
+});
+
 
 // Port
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
+
